42e4bd76346cfd4b65c797d04eee04ab58542c96
Enzo AICARDI <eaicardi@datasolution.fr>
2024-01-17 13:57:19 +0100

    [config] change folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,18 @@
 import terser from "@rollup/plugin-terser";
 
+/** @type {[{format: string, ext: string}]} */
 const formats = [
-    ["iife", "prod"],
-    ["esm", "prod"],
-    ["iife", "dev"],
-    ["esm", "dev"],
+    ["iife", ""],
+    ["esm", ""],
+    ["iife", ".dev"],
+    ["esm", ".dev"],
 ];
 
-export default formats.map(function ([format, mode]) {
+export default formats.map(function ([format, ext]) {
     return {
         input: "src/bundle.js",
         output: {
-            file: `dist${mode === "dev" ? "/dev" : ""}/vif${
-                format === "iife" ? ".iife" : ""
-            }.js`,
+            file: `dist/${format}/vif${ext}.js`,
             format: format,
             name: "Vif",
         },
@@ -35,7 +34,7 @@ export default formats.map(function ([format, mode]) {
                     },
                 },
                 output: {
-                    comments: mode === "dev",
+                    comments: ext === ".dev",
                 },
             }),
         ],
